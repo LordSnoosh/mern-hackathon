53a49e0553f23a113439e18d4c08c3f2fc319276
jackhr <jc2o@mac.com>
2021-05-06 17:16:23 -0400

    add a bunch of orders

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -1,18 +1,18 @@
-import './OrderList.css';
-import OrderListItem from '../OrderListItem/OrderListItem';
+// import './OrderList.css';
+import OrderListItem from '../OrderLIstItem/OrderListItem';
 
 export default function OrderList({ orders, activeOrder, handleSelectOrder }) {
-  const orderItems = orders.map(ord =>
-    <OrderListItem
-      order={ord}
-     isSelected={ord === activeOrder}
-     handleSelectOrder={handleSelectOrder}
-     key={ord._id}
-    />
-  );
+  // const orderItems = orders.map(ord =>
+  //   <OrderListItem
+  //     order={ord}
+  //    isSelected={ord === activeOrder}
+  //    handleSelectOrder={handleSelectOrder}
+  //    key={ord._id}
+  //   />
+  // );
   return (
     <main className="OrderList">
-      {orderItems.length ? orderItems : <span className="noOrders"> No Order History</span>}
+      {/* {orderItems.length ? orderItems : <span className="noOrders"> No Order History</span>} */}
     </main>
   );
 }
